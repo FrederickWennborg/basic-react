a8acf517c2848c5f29182ae8d05d441bce0683f3
FrederickWennborg <frederick.wennborg@systementor.se>
2026-01-18 09:29:58 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Routes from 'react-router'
 
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
-      <Routes>
-
-      </Routes>
+    
     </>
   )
 }
